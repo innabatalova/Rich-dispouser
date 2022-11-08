8addf781f4c257a9b9f691fcbd3669f5108cb204
Inna Batalova <batalovainna97@gmail.com>
2022-11-08 15:33:42 +0300

    Create: added slider-bottom

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-  $(".carousel").owlCarousel();
+  $(".carousel-top").owlCarousel();
 });
 
-$(".carousel").owlCarousel({
+$(document).ready(function(){
+  $(".carousel-bottom").owlCarousel();
+});
+
+$(".carousel-top").owlCarousel({
     items: 1,
     margin: -40,
     loop: true,
@@ -10,10 +14,35 @@ $(".carousel").owlCarousel({
     nav: true,
     touchDrag: true,
     smartSpeed: 850,
-    navContainer: ".carousel-item__nav",
+    navContainer: ".carousel-item__nav_top",
     navText: [
-      "<img src='img/arrow-top.svg' class='carousel-item__prev'>",
-      "<img src='img/arrow-top.svg' class='carousel-item__next'>",
+      "<img src='img/arrow-top.svg' class='carousel-top__prev'>",
+      "<img src='img/arrow-top.svg' class='carousel-top__next'>",
+    ],
+    responsive: {
+      320: {
+        items: 1,
+        margin: 20,
+      },
+      1150: {
+        items: 1,
+        margin: 0,
+      },
+    },
+  });
+
+$(".carousel-bottom").owlCarousel({
+    items: 1,
+    margin: -40,
+    loop: true,
+    dots: false,
+    nav: true,
+    touchDrag: true,
+    smartSpeed: 850,
+    navContainer: ".carousel-item__nav_bottom",
+    navText: [
+      "<img src='img/arrow-bottom.svg' class='carousel-bottom__prev'>",
+      "<img src='img/arrow-bottom.svg' class='carousel-bottom__next'>",
     ],
     responsive: {
       320: {
